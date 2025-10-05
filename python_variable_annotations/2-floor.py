7497574fd6ec1275a06e2c23819998a9717c18ce
Rayan Hantoul <ryan-055@outlook.com>
2025-10-05 17:10:50 +0300

    task 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import math
+"""
+defain function add
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Return the floor of a floating-point number.
+
+    The floor of a number is the largest integer less than or equal to it.
+
+    Args:
+        n (float): The number to be floored.
+
+    Returns:
+        int: The floor of the number.
+    """
+    return math.floor(n)
