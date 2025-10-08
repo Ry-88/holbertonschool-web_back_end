30c2a785c477db2d316be06bb8c084d9c6332a2c
Rayan Hantoul <ryan-055@outlook.com>
2025-10-08 18:14:37 +0300

    fix pycodestyle

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -4,7 +4,9 @@
 
 from typing import Union, Tuple
 
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """function to_kv that takes a string k and an int OR float v as arguments and returns a tuple"""
+    """function to_kv that takes a string k and an int OR
+     float v as arguments and returns a tuple"""
 
     return (k, float(v ** 2))
