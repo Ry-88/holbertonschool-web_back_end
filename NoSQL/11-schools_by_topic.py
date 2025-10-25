e46b9b1c8511c36cf21a52bcc92595b336b82253
Rayan Hantoul <ryan-055@outlook.com>
2025-10-25 23:05:41 +0300

    task 11

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""function that returns the list of school having a specific topic"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """Method to return a list of topics"""
+
+    return mongo_collection.find({'topics': topic})
