39ffc00f1fe152f20aff496e4bc34818079f3c64
Rayan Hantoul <ryan-055@outlook.com>
2025-10-26 18:07:17 +0300

    task 100 advanced

---
+++ python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Module that defines a function for safely returning the first element of a list."""
+
+from typing import Any, Sequence, Optional
+
+
+def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
+    """
+    Return the first element of a sequence if it exists, otherwise None.
+
+    Args:
+        lst (Sequence[Any]): A sequence (e.g., list, tuple) of unknown element types.
+
+    Returns:
+        Optional[Any]: The first element if available, otherwise None.
+    """
+    if lst:
+        return lst[0]
+    else:
+        return None
