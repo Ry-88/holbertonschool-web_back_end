d3b37c0a96f64fd7b729d35914e59303c7d7f11f
Rayan Hantoul <ryan-055@outlook.com>
2025-11-01 22:28:12 +0300

    fix code

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -18,6 +18,6 @@ export default class SkyHighBuilding extends Building {
 
   // Override evacuationWarningMessage
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors.`;
+    return `Evacuate slowly the ${this._floors} floors`;
   }
 }
