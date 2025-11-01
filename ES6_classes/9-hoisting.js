afeacdbdd893fa047f678a576102e05aac62948b
Rayan Hantoul <ryan-055@outlook.com>
2025-11-01 22:40:24 +0300

    fix code

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -33,7 +33,7 @@ export class StudentHolberton {
   }
 }
 
-// Now create HolbertonClass instances
+// Create HolbertonClass instances
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
@@ -44,4 +44,5 @@ const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
+// Export the array **after** all students are created
 export const listOfStudents = [student1, student2, student3, student4, student5];
