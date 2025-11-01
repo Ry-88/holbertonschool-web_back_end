54079e39acb483361a5b649ed4067c718f849644
Rayan Hantoul <ryan-055@outlook.com>
2025-11-01 22:35:43 +0300

    task 8

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,33 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    if (typeof size !== 'number') {
+      throw new TypeError('size must be a number');
+    }
+    if (typeof location !== 'string') {
+      throw new TypeError('location must be a string');
+    }
+
+    this._size = size;
+    this._location = location;
+  }
+
+  // Getter for size
+  get size() {
+    return this._size;
+  }
+
+  // Getter for location
+  get location() {
+    return this._location;
+  }
+
+  // When cast to Number
+  valueOf() {
+    return this._size;
+  }
+
+  // When cast to String
+  toString() {
+    return this._location;
+  }
+}
