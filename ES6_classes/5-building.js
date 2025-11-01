1c5a709afbc538a10f407f6bd6e9947bcab6e169
Rayan Hantoul <ryan-055@outlook.com>
2025-11-01 22:14:03 +0300

    fix code

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,17 +1,14 @@
 export default class Building {
   constructor(sqft) {
-    if (new.target === Building) {
-      throw new Error('Cannot instantiate abstract class Building directly');
-    }
-
     if (typeof sqft !== 'number') {
       throw new TypeError('sqft must be a number');
     }
 
     this._sqft = sqft;
 
-    // Check if the extending class implements evacuationWarningMessage
-    if (typeof this.evacuationWarningMessage !== 'function') {
+    // Only check for subclasses, not the base class itself
+    const isBaseClass = this.constructor === Building;
+    if (!isBaseClass && typeof this.evacuationWarningMessage !== 'function') {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
   }
