c785ab9bbc02a874d75db4d52ddf8a221d3768dc
Rayan Hantoul <ryan-055@outlook.com>
2025-11-01 22:10:11 +0300

    task 5

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,23 @@
+export default class Building {
+  constructor(sqft) {
+    if (new.target === Building) {
+      throw new Error('Cannot instantiate abstract class Building directly');
+    }
+
+    if (typeof sqft !== 'number') {
+      throw new TypeError('sqft must be a number');
+    }
+
+    this._sqft = sqft;
+
+    // Check if the extending class implements evacuationWarningMessage
+    if (typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+
+  // Getter for sqft
+  get sqft() {
+    return this._sqft;
+  }
+}
