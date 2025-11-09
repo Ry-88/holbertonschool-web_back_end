02874c5337030963649cccc0ef39433e0c1ef05a
Rayan Hantoul <ryan-055@outlook.com>
2025-11-09 20:50:50 +0300

    fix code

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -3,10 +3,9 @@ import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => {
-      return results.map((result) => ({
+    .then((results) => 
+        results.map((result) => ({
         status: result.status,
         value: result.status === 'fulfilled' ? result.value : result.reason,
-      }));
-    });
+      })));
 }
