56385f4c4987759d3137b7f5621a9305eb5fb62d
Rayan Hantoul <ryan-055@outlook.com>
2025-11-12 21:16:48 +0300

    fix code

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,7 +2,11 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
+  process.stdout.write(`Your name is: ${name}`);
+});
+
+process.stdin.on('exit', () => {
+  console.log('This important software is now closing\n');
 });
 
 process.on('SIGINT', () => {
