eb90a4312020f87bdc6c07fc3b29b8dad9661c17
Rayan Hantoul <ryan-055@outlook.com>
2025-11-12 19:34:55 +0300

    fix code

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -5,6 +5,6 @@ process.stdin.on('data', (data) => {
   process.stdout.write(`Your name is: ${name}\n`);
 });
 
-process.on('exit', () => {
+process.stdin.on('exit', () => {
   console.log('This important software is now closing');
 });
