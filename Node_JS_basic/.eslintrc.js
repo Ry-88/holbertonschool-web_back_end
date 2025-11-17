9c57a3840a875f254fa8deac58c03e852e0f368c
Rayan Hantoul <ryan-055@outlook.com>
2025-11-17 19:18:17 +0300

    fix code

--- Node_JS_basic/.eslintrc.js
+++ Node_JS_basic/.eslintrc.js
@@ -2,36 +2,13 @@ module.exports = {
   env: {
     browser: false,
     es6: true,
-    jest: true,
+    node: true,
   },
-  extends: [
-    'airbnb-base',
-    'plugin:jest/all',
-  ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
+  extends: ['airbnb-base'],
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: ['jest'],
   rules: {
-    'max-classes-per-file': 'off',
-    'no-underscore-dangle': 'off',
-    'no-console': 'off',
-    'no-shadow': 'off',
-    'no-restricted-syntax': [
-      'error',
-      'LabeledStatement',
-      'WithStatement',
-    ],
   },
-  overrides:[
-    {
-      files: ['*.js'],
-      excludedFiles: 'babel.config.js',
-    }
-  ]
 };
